a4b681f5051f01bd20cd3f39e8812c4c8d8f9312
monatru3 <monatru3@hotmail.com>
2015-10-18 20:02:23 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def create(name)
+    @name=name
+  end
+  def name
+    @name
+  end
+
+  def name=(insert_name)
+    @name=insert_name
+  end
+
+      def create(breed)
+    @breed=breed
+  end
+  def breed
+    @breed
+  end
+
+  def breed=(insert_breed)
+    @breed=insert_breed
+end
+end
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+  def create(name)
+    @name=name
+  end
+  def name
+    @name
+  end
+
+  def name=(insert_name)
+    @name=insert_name
+  end
+
+      def create(job)
+    @job=job
+  end
+  def job
+    @job
+  end
+
+  def job=(insert_job)
+    @job=insert_job
+end
+end
